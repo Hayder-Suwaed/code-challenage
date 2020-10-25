d8d201bf2699ba44aa69ca80e56ed3a3fb66be4f
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-10-24 22:07:18 -0600

    code challenge

---
+++ models/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb://localhost:27017/CrudDB",
+  { useNewUrlParser: true },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB Connection Succeeded.");
+    } else {
+      console.log("MongoDB Connection : " + err);
+    }
+  }
+);
+
+
+require('./appointment.model');
